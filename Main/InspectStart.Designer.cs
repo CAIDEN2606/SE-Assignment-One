57ed312ea7715cbd989951ecf0a8fc64335e0a24
labujaber <labujaber1@sky.com>
2021-10-07 14:28:56 +0100

    Create an ispector form with som labels and txt things

---
+++ Main/InspectStart.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Main
+{
+    partial class InspectStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.lbl_inspectorName = new System.Windows.Forms.Label();
+            this.txt_InspectorName = new System.Windows.Forms.TextBox();
+            this.txt_siteName = new System.Windows.Forms.TextBox();
+            this.txt_workArea = new System.Windows.Forms.TextBox();
+            this.txt_jobDescription = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txt_type = new System.Windows.Forms.TextBox();
+            this.txt_supervisor = new System.Windows.Forms.TextBox();
+            this.lbl_SiteName = new System.Windows.Forms.Label();
+            this.lbl_workArea = new System.Windows.Forms.Label();
+            this.lbl_date = new System.Windows.Forms.Label();
+            this.lbl_jobDescription = new System.Windows.Forms.Label();
+            this.lbl_type = new System.Windows.Forms.Label();
+            this.lbl_supervisor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Location = new System.Drawing.Point(268, 26);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(116, 13);
+            this.lbl_title.TabIndex = 0;
+            this.lbl_title.Text = "Safety Inspection Audit\r\n";
+            // 
+            // lbl_inspectorName
+            // 
+            this.lbl_inspectorName.AutoSize = true;
+            this.lbl_inspectorName.Location = new System.Drawing.Point(200, 87);
+            this.lbl_inspectorName.Name = "lbl_inspectorName";
+            this.lbl_inspectorName.Size = new System.Drawing.Size(70, 13);
+            this.lbl_inspectorName.TabIndex = 1;
+            this.lbl_inspectorName.Text = "Inspection by";
+            // 
+            // txt_InspectorName
+            // 
+            this.txt_InspectorName.Location = new System.Drawing.Point(292, 80);
+            this.txt_InspectorName.Name = "txt_InspectorName";
+            this.txt_InspectorName.Size = new System.Drawing.Size(204, 20);
+            this.txt_InspectorName.TabIndex = 2;
+            // 
+            // txt_siteName
+            // 
+            this.txt_siteName.Location = new System.Drawing.Point(292, 122);
+            this.txt_siteName.Name = "txt_siteName";
+            this.txt_siteName.Size = new System.Drawing.Size(204, 20);
+            this.txt_siteName.TabIndex = 3;
+            // 
+            // txt_workArea
+            // 
+            this.txt_workArea.Location = new System.Drawing.Point(292, 162);
+            this.txt_workArea.Name = "txt_workArea";
+            this.txt_workArea.Size = new System.Drawing.Size(204, 20);
+            this.txt_workArea.TabIndex = 4;
+            // 
+            // txt_jobDescription
+            // 
+            this.txt_jobDescription.Location = new System.Drawing.Point(292, 243);
+            this.txt_jobDescription.Name = "txt_jobDescription";
+            this.txt_jobDescription.Size = new System.Drawing.Size(204, 20);
+            this.txt_jobDescription.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(296, 203);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // txt_type
+            // 
+            this.txt_type.Location = new System.Drawing.Point(292, 283);
+            this.txt_type.Name = "txt_type";
+            this.txt_type.Size = new System.Drawing.Size(204, 20);
+            this.txt_type.TabIndex = 7;
+            // 
+            // txt_supervisor
+            // 
+            this.txt_supervisor.Location = new System.Drawing.Point(292, 322);
+            this.txt_supervisor.Name = "txt_supervisor";
+            this.txt_supervisor.Size = new System.Drawing.Size(204, 20);
+            this.txt_supervisor.TabIndex = 8;
+            // 
+            // lbl_SiteName
+            // 
+            this.lbl_SiteName.AutoSize = true;
+            this.lbl_SiteName.Location = new System.Drawing.Point(200, 129);
+            this.lbl_SiteName.Name = "lbl_SiteName";
+            this.lbl_SiteName.Size = new System.Drawing.Size(56, 13);
+            this.lbl_SiteName.TabIndex = 9;
+            this.lbl_SiteName.Text = "Site Name";
+            // 
+            // lbl_workArea
+            // 
+            this.lbl_workArea.AutoSize = true;
+            this.lbl_workArea.Location = new System.Drawing.Point(200, 162);
+            this.lbl_workArea.Name = "lbl_workArea";
+            this.lbl_workArea.Size = new System.Drawing.Size(58, 13);
+            this.lbl_workArea.TabIndex = 10;
+            this.lbl_workArea.Text = "Work Area";
+            // 
+            // lbl_date
+            // 
+            this.lbl_date.AutoSize = true;
+            this.lbl_date.Location = new System.Drawing.Point(200, 209);
+            this.lbl_date.Name = "lbl_date";
+            this.lbl_date.Size = new System.Drawing.Size(30, 13);
+            this.lbl_date.TabIndex = 11;
+            this.lbl_date.Text = "Date";
+            // 
+            // lbl_jobDescription
+            // 
+            this.lbl_jobDescription.AutoSize = true;
+            this.lbl_jobDescription.Location = new System.Drawing.Point(200, 246);
+            this.lbl_jobDescription.Name = "lbl_jobDescription";
+            this.lbl_jobDescription.Size = new System.Drawing.Size(78, 13);
+            this.lbl_jobDescription.TabIndex = 12;
+            this.lbl_jobDescription.Text = "Job description";
+            // 
+            // lbl_type
+            // 
+            this.lbl_type.AutoSize = true;
+            this.lbl_type.Location = new System.Drawing.Point(200, 286);
+            this.lbl_type.Name = "lbl_type";
+            this.lbl_type.Size = new System.Drawing.Size(31, 13);
+            this.lbl_type.TabIndex = 13;
+            this.lbl_type.Text = "Type";
+            // 
+            // lbl_supervisor
+            // 
+            this.lbl_supervisor.AutoSize = true;
+            this.lbl_supervisor.Location = new System.Drawing.Point(200, 325);
+            this.lbl_supervisor.Name = "lbl_supervisor";
+            this.lbl_supervisor.Size = new System.Drawing.Size(57, 13);
+            this.lbl_supervisor.TabIndex = 14;
+            this.lbl_supervisor.Text = "Supervisor";
+            // 
+            // InspectStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_supervisor);
+            this.Controls.Add(this.lbl_type);
+            this.Controls.Add(this.lbl_jobDescription);
+            this.Controls.Add(this.lbl_date);
+            this.Controls.Add(this.lbl_workArea);
+            this.Controls.Add(this.lbl_SiteName);
+            this.Controls.Add(this.txt_supervisor);
+            this.Controls.Add(this.txt_type);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txt_jobDescription);
+            this.Controls.Add(this.txt_workArea);
+            this.Controls.Add(this.txt_siteName);
+            this.Controls.Add(this.txt_InspectorName);
+            this.Controls.Add(this.lbl_inspectorName);
+            this.Controls.Add(this.lbl_title);
+            this.Name = "InspectStart";
+            this.Text = "InspectStart";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Label lbl_inspectorName;
+        private System.Windows.Forms.TextBox txt_InspectorName;
+        private System.Windows.Forms.TextBox txt_siteName;
+        private System.Windows.Forms.TextBox txt_workArea;
+        private System.Windows.Forms.TextBox txt_jobDescription;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txt_type;
+        private System.Windows.Forms.TextBox txt_supervisor;
+        private System.Windows.Forms.Label lbl_SiteName;
+        private System.Windows.Forms.Label lbl_workArea;
+        private System.Windows.Forms.Label lbl_date;
+        private System.Windows.Forms.Label lbl_jobDescription;
+        private System.Windows.Forms.Label lbl_type;
+        private System.Windows.Forms.Label lbl_supervisor;
+    }
+}
